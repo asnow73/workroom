c1113b623763867475827d13423354807f07ba60
asnow73 <albertulgu@list.ru>
2013-11-05 18:30:51 +0000

    Disable SSL for prodaction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ require "net/http"
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "WorkRoom"
+    base_title = "AlbertRoom"
     if page_title.empty?
       base_title
     else
